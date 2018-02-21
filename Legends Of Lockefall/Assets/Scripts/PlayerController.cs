e268b6a1de23d89f6fa071b466c0ba23b7d992de
Fun Sized Popcorn <funSizedPopcorn@gmail.com>
2018-02-21 17:53:34 -0500

    Added basic player movement
    Something's being weird with the amount of files changed

---
+++ Legends Of Lockefall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Animator anim;
+
+    public float moveSpeed;
+    public bool moving;
+    private Vector2 lastMove;
+
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+
+	void Update ()
+    {
+        moving = false;
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        
+        if(horizontal != 0)
+        {
+            transform.Translate(new Vector3(horizontal * moveSpeed * Time.deltaTime, 0f, 0f));
+            moving = true;
+            lastMove = new Vector2(horizontal, 0f);
+        }
+
+        if (vertical != 0)
+        {
+            transform.Translate(new Vector3(0f, vertical * moveSpeed * Time.deltaTime, 0f));
+            moving = true;
+            lastMove = new Vector2(0f, vertical);
+        }
+
+        anim.SetFloat("Vertical", vertical);
+        anim.SetFloat("Horizontal", horizontal);
+        anim.SetFloat("LastMoveVertical", lastMove.y);
+        anim.SetFloat("LastMoveHorizontal", lastMove.x);
+        anim.SetBool("Moving", moving);
+    }
+}
