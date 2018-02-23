5b4a7507594d4d3bea0ec24d41cb62ea82190a0c
Fun Sized Popcorn <funSizedPopcorn@gmail.com>
2018-02-23 16:42:46 -0500

    Added hearts

--- Legends Of Lockefall/Assets/Scripts/PlayerStats.cs
+++ Legends Of Lockefall/Assets/Scripts/PlayerStats.cs
@@ -5,28 +5,75 @@ using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour {
 
-    public int maxHealth;
+    public int maxHealth = 3;
     public int currentHealth;
-    public Slider healthBar;
+
+    public Transform heartParent;
+    Image[] hearts;
+    public GameObject heartPrefab;
+    public Sprite fullHeart, emptyHeart;
+
+    public delegate void OnHeartsChanged();
+    public OnHeartsChanged onHeartsChangedCallback;
     
 	void Start ()
     {
         currentHealth = maxHealth;
+        onHeartsChangedCallback += UpdateHearts;
+        UpdateHearts();
 	}
 	
 	void Update ()
     {
-        healthBar.maxValue = maxHealth;
-        healthBar.value = currentHealth;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        maxHealth = Mathf.Clamp(maxHealth, 3, 19);
 
         if(Input.GetKeyDown(KeyCode.T))
         {
-            TakeDamage(5);
+            TakeDamage(1);
+            onHeartsChangedCallback();
         }
-	}
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            TakeDamage(-1);
+            onHeartsChangedCallback();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            maxHealth++;
+        }
+
+        if (hearts.Length < maxHealth)
+        {
+            GameObject newHeart;
+            newHeart = Instantiate(heartPrefab);
+            newHeart.transform.SetParent(heartParent);
+
+            onHeartsChangedCallback();
+        }
+    }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+    }
+
+    void UpdateHearts()
+    {
+        hearts = heartParent.GetComponentsInChildren<Image>();
+
+        for(int i = 0; i < hearts.Length; i++)
+        {
+            if(i < currentHealth)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+        }
     }
 }
