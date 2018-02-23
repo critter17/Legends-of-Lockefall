f9769f930ff2e60a48589f21af4bfa2ca237a14e
Fun Sized Popcorn <funSizedPopcorn@gmail.com>
2018-02-22 20:44:35 -0500

    Revert "Merge branch 'master' of https://github.com/Fun-Sized-Popcorn/Legends-Of-Lockefall into Branch_f60c01d"
    This reverts commit fab3ad78248c9dcf4b70d6c9abe4be44aa64745e, reversing
    changes made to c11d01bde0b3f07f5f8e16d063e74e611660f09c.

--- Legends Of Lockefall/Assets/Scripts/PlayerController.cs
+++ Legends Of Lockefall/Assets/Scripts/PlayerController.cs
@@ -8,11 +8,13 @@ public class PlayerController : MonoBehaviour {
 
     public float moveSpeed;
     public bool moving;
-    private Vector2 lastMove;
+    public Vector2 lastMove;
+    public bool canMove = true;
 
 	void Start ()
     {
         anim = GetComponent<Animator>();
+        lastMove = new Vector2(0, -1);
 	}
 
 	void Update ()
@@ -20,19 +22,22 @@ public class PlayerController : MonoBehaviour {
         moving = false;
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        
-        if(horizontal != 0)
-        {
-            transform.Translate(new Vector3(horizontal * moveSpeed * Time.deltaTime, 0f, 0f));
-            moving = true;
-            lastMove = new Vector2(horizontal, 0f);
-        }
 
-        if (vertical != 0)
+        if (canMove)
         {
-            transform.Translate(new Vector3(0f, vertical * moveSpeed * Time.deltaTime, 0f));
-            moving = true;
-            lastMove = new Vector2(0f, vertical);
+            if (horizontal != 0)
+            {
+                transform.Translate(new Vector3(horizontal * moveSpeed * Time.deltaTime, 0f, 0f));
+                moving = true;
+                lastMove = new Vector2(horizontal, 0f);
+            }
+
+            if (vertical != 0)
+            {
+                transform.Translate(new Vector3(0f, vertical * moveSpeed * Time.deltaTime, 0f));
+                moving = true;
+                lastMove = new Vector2(0f, vertical);
+            }
         }
 
         anim.SetFloat("Vertical", vertical);
@@ -40,5 +45,10 @@ public class PlayerController : MonoBehaviour {
         anim.SetFloat("LastMoveVertical", lastMove.y);
         anim.SetFloat("LastMoveHorizontal", lastMove.x);
         anim.SetBool("Moving", moving);
+    }
+
+    public void CanMove(bool state)
+    {
+        canMove = state;
     }
 }
